5e0c67a51198f033c9a0723577dbbfbc837befc0
NIKITHA REDDY <50329516+bnikitha07@users.noreply.github.com>
2020-01-12 00:14:21 +0530

    Add files via upload
    finds the first recurring element

---
+++ first_recurring.py
@@ -0,0 +1,19 @@
+def recurring(f):
+    a=list(f)
+    b="none"
+    for i in a:
+        k=f.count(i)
+        if k>1:
+            b=i
+            break
+    return b
+
+
+
+foo=input("please enter the name")
+f=str(foo)
+print(f)
+a=list(f)
+print(a)
+print(recurring(f))
+
